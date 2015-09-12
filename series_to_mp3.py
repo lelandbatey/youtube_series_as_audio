048a1afa657a603c589b5eac9e43adcdfdf0d08e
lelandbatey <lelandbatey@gmail.com>
2015-09-12 14:33:10 -0700

    Begin cleansing of series_to_mp3.py

--- series_to_mp3.py
+++ series_to_mp3.py
@@ -49,74 +49,12 @@ def strip_ext(filename):
     """Removes the extension from a filename."""
     return ''.join(filename.split('.')[:-1])
 
-def download_series(series_url, series_alias):
-    """Downloads a given youtube series."""
-    download_cmd = \
-    'youtube-dl --force-ipv4 -f "141/140" "{}" -o "./{}/video/%(autonumber)s__%(title)s.%(ext)s"'
-    download_cmd = download_cmd.format(series_url, series_alias)
-
-    return download_cmd
-
-def extract_audio_command_builder(series_alias):
-    """Builds the commands to be run out of the list of files."""
-    ggfiles = get_files('./'+series_alias+'/video', '.m4a')
-
-    convertcommands = []
-    for index, value in enumerate(ggfiles):
-        convertcommands.append('ffmpeg -i '+\
-        '"./{2}/video/{1}" -vn -ac 2 -ar 44100 -ab 192k -f mp3 "./{2}/audio/{3}_192k.mp3"'\
-        .format(index+1, value, series_alias, strip_ext(value)))
-
-    return ' && '.join(convertcommands)
-
-def build_cmd_extract_aac(series_alias):
-    """Builds command to extract aac from video files."""
-    vfiles = get_files('./'+series_alias+'/video')
-
-    convertcommands = []
-    for vid in vfiles:
-        convertcommands.append('ffmpeg -i '+\
-        '"./{1}/video/{0}" -vn -acodec copy "./{1}/audio/{2}.m4a"'.format(\
-            vid, series_alias, strip_ext(vid)))
-    return ' && '.join(convertcommands)
-
-def build_concat_aac(series_alias):
-    """Concatenates aac files. Right now this doesn't work, I don't know why."""
-    afiles = get_aac_audio_files(series_alias)
-    command = 'ffmpeg -i "concat:{}" -acodec copy {}/complete_{}.m4a'
-    command = command.format('|'.join(afiles), series_alias, series_alias)
-    return command
-
-
-def concat_audio_cmd_builder(series_alias):
-    """Concatenates all mp3 files in a directory together."""
-    mp3s = get_mpg_audio_files(series_alias)
-    print(mp3s)
-
-    command = 'ffmpeg -i "concat:{}" -acodec copy {}/complete_{}.mp3'
-    command = command.format('|'.join(mp3s), series_alias, series_alias)
-    
-    return command
-
-def create_dirs(series_alias):
-    """Creates the proper directory structure based on series alias."""
-    dirlist = [
-        './{}/',
-        './{}/video/',
-        './{}/audio'
-    ]
-
-    dirlist = [dirct.format(series_alias) for dirct in dirlist]
-
-    for dirct in dirlist:
-        if not os.path.exists(dirct):
-            os.makedirs(dirct)
-
-    return
 
 def seperate_mp3_format(series_alias, series_full_name=''):
     """Builds markdown for the separate mp3s"""
-    # output = ""
+
+    if not series_full_name: series_full_name = series_alias
+
     ziplocation = series_alias+'/audio/'+series_alias+'.zip'
     fmt = u"""
     - [All *{0}* separate audio files in one zip ]({1}{2}) -- {3}
@@ -125,15 +63,10 @@ def seperate_mp3_format(series_alias, series_full_name=''):
     """.format(\
         series_full_name, BASEURL, ziplocation,\
         sizeof_fmt(os.path.getsize(ziplocation)), series_alias+'/audio/')
-    # for afile in get_mpg_audio_files(series_alias):
-    #     output += fmt.format(strip_ext(os.path.basename(afile)),\
-    #         BASEURL, afile, sizeof_fmt(os.path.getsize(afile)))
-
     return fmt
 
 
-
-def build_markdown(series_alias, series_full_name=''):
+def build_markdown(series_alias, series_full_name=None):
     """Creates markdown for information about the series."""
 
     if not series_full_name:
@@ -155,11 +88,108 @@ def build_markdown(series_alias, series_full_name=''):
 
     return info
 
+
+def build_cmd_extract_aac(series_alias):
+    """Builds command to extract aac from video files."""
+    vfiles = get_files('./'+series_alias+'/video')
+
+    convertcommands = []
+    for vid in vfiles:
+        convertcommands.append('ffmpeg -i '+\
+        '"./{1}/video/{0}" -vn -acodec copy "./{1}/audio/{2}.m4a"'.format(\
+            vid, series_alias, strip_ext(vid)))
+    return ' && '.join(convertcommands)
+
+def build_concat_aac(series_alias):
+    """Concatenates aac files. Right now this doesn't work, I don't know why."""
+    afiles = get_aac_audio_files(series_alias)
+    command = 'ffmpeg -i "concat:{}" -acodec copy {}/complete_{}.m4a'
+    command = command.format('|'.join(afiles), series_alias, series_alias)
+    return command
+
+
+def create_dirs(series_alias):
+    """Based on the given series_alias, creates the proper cooresponding directory structure."""
+    dirlist = [
+        './{}/',
+        './{}/video/',
+        './{}/audio'
+    ]
+
+    dirlist = [given_dir.format(series_alias) for given_dir in dirlist]
+
+    # Create a the directories (if they don't exist)
+    for given_dir in dirlist:
+        if not os.path.exists(given_dir):
+            os.makedirs(given_dir)
+
+
+def download_series(series_url, series_alias):
+    """Downloads a given youtube series into the `series_alias/video/` folder."""
+    download_cmd = \
+    'youtube-dl --force-ipv4 -f "141/140" "{}" -o "./{}/video/%(autonumber)s__%(title)s.%(ext)s"'
+    download_cmd = download_cmd.format(series_url, series_alias)
+    return download_cmd
+
+
+def transcode_source_to_new_audio(series_alias, audio_fmt='mp3'):
+    """Creates shell command to derive mp3 audio from existing downloaded media."""
+    ggfiles = get_files('./'+series_alias+'/video', '.m4a')
+
+    convertcommands = []
+    def mpg_transcode(value, series_alias):
+        cmd = 'ffmpeg -i '+\
+        '"./{series_alias}/video/{source_file}" '+\
+        '-vn -ac 2 -ar 44100 ab 192k -f mp3 '+\
+        '"./{series_alias}/audio/{source_file_name}_192k.mp3"'
+
+        cmd = cmd.format(source_file=value,
+                         series_alias=series_alias,
+                         source_file_name=strip_ext(value))
+
+        return cmd
+    def aac_copy(value, series_alias):
+        cmd = 'cp "./{series_alias}/video/{source_file}" '+\
+        '"./{series_alias}/audio/{source_file}"'
+        cmd = cmd.format(source_file=value, series_alias=series_alias)
+        return cmd
+    for index, value in enumerate(ggfiles):
+        delegate = { 'mp3': mpg_transcode,
+                     'aac': aac_copy
+                   }[audio_fmt]
+        convertcommands.append(delegate(value, series_alias))
+
+    return ' && '.join(convertcommands)
+
+
+def concat_audio_cmd_builder(series_alias):
+    """Concatenates all mp3 files in a directory together."""
+    mp3s = get_mpg_audio_files(series_alias)
+    print(mp3s)
+
+    command = 'ffmpeg -i "concat:{}" -acodec copy {}/complete_{}.mp3'
+    command = command.format('|'.join(mp3s), series_alias, series_alias)
+
+    return command
+
+
 def zip_separate(series_alias):
     """Zips up all the separately created audio files."""
-    cmdstr = 'zip "{0}/audio/{0}.zip" {0}/audio/*.mp3'.format(series_alias)
+    cmdstr = 'zip "{0}/audio/{0}.zip" {0}/audio/*'.format(series_alias)
     return cmdstr
 
+
+def create_audio_for_series(series_alias, series_url, audio_format='mp3'):
+    """Creates an audio series for the given youtube series."""
+    # Make the directories
+    create_dirs(series_alias)
+
+    run_command(download_series(series_url, series_alias))
+    run_command(transcode_source_to_new_audio(series_alias, audio_format))
+    run_command(zip_separate(series_alias))
+    run_command(concat_audio_cmd_builder(series_alias))
+
+    return
 
 def main():
     """Controls flow of program through procedure."""
@@ -173,17 +203,7 @@ def main():
     else:
         series_alias, series_url = sys.argv[1], sys.argv[2]
 
-    # Make the directories
-    create_dirs(series_alias)
-    
-    run_command(download_series(series_url, series_alias))
-    run_command(extract_audio_command_builder(series_alias))
-    run_command(concat_audio_cmd_builder(series_alias))
-    run_command(zip_separate(series_alias))
-
-    # run_command(build_cmd_extract_aac(series_alias))
-    # run_command(build_concat_aac(series_alias))
-    
+    create_audio_for_series(series_alias, series_url, audio_format='mp3')
     print(build_markdown(series_alias))
 
 
